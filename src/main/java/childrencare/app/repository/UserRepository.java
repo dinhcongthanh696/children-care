5527742703603f798c073e52e33d7f3a1f295744
Newt1712 <ankvhe150757@fpt.edu.vn>
2022-02-11 23:10:34 +0700

    getDoctor and service

--- src/main/java/childrencare/app/repository/UserRepository.java
+++ src/main/java/childrencare/app/repository/UserRepository.java
@@ -1,12 +1,19 @@
 package childrencare.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import childrencare.app.model.UserModel;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer>{
 	
 	public UserModel findByEmail(String email);
+
+	@Query(value = "Select * from user_model u inner join role r " +
+			"on u.role_id = r.role_id where r.role_name = ?1", nativeQuery = true)
+	public List<UserModel> findUserModelByUserRole(String roleName);
 }
--- src/main/java/childrencare/app/service/UserService.java
+++ src/main/java/childrencare/app/service/UserService.java
@@ -1,8 +1,20 @@
 package childrencare.app.service;
 
+import childrencare.app.model.UserModel;
+import childrencare.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
-	
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserModel> findAllDoctor(){
+        return userRepository.findUserModelByUserRole("doctor");
+    }
+
 }
