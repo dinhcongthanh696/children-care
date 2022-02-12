d2566a310f083b34c9d64cc2d34f389ce0370a9a
Newt1712 <ankvhe150757@fpt.edu.vn>
2022-02-12 21:50:35 +0700

    get Services

--- src/main/java/childrencare/app/repository/UserRepository.java
+++ src/main/java/childrencare/app/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import childrencare.app.model.UserModel;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,4 +17,11 @@ public interface UserRepository extends JpaRepository<UserModel, Integer>{
 	@Query(value = "Select * from user_model u inner join role r " +
 			"on u.role_id = r.role_id where r.role_name = ?1", nativeQuery = true)
 	public List<UserModel> findUserModelByUserRole(String roleName);
+
+	@Query(value = "Update user_model set fullname = ?1, phone = ?2, gender = ?3\n" +
+			"where email = ?4", nativeQuery = true)
+	public void updateInfo(String fullName, String phone, boolean gender, String email);
+
+
+//	public List<UserModel> getAvailableDoctor(Date date);
 }
