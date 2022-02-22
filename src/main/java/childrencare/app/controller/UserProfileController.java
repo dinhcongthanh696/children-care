f1be4fe358f081b7c8f16da09664856df1ed8879
NguyenTheThai <thainthe150042@fpt.edu.vn>
2022-02-22 23:47:14 +0700

    user profile 2

--- src/main/java/childrencare/app/controller/UserProfileController.java
+++ src/main/java/childrencare/app/controller/UserProfileController.java
@@ -5,26 +5,30 @@ import childrencare.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/resetpass")
-public class ResetPasswordController {
-    @Autowired
-    UserRepository userRepository;
+@RequestMapping("/profile")
+public class UserProfileController {
 
-    @PostMapping("/doreset")
-    public String doReset(@RequestParam (name = "password") String password, HttpSession session){
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/update")
+    public String updateProfile(@RequestParam(name = "address") String address, @RequestParam(name = "fullname") String fullname,
+                                @RequestParam(name = "phone") String phone, HttpSession session){
         UserModel user = (UserModel) session.getAttribute("user");
-        if(user != null) {
-            user.setPassword(password);
+        if(user != null){
+            user.setAddress(address);
+            user.setFullname(fullname);
+            user.setPhone(phone);
             userRepository.save(user);
-            return "redirect:/";
+            return "redirect:/profile/update";
         }
-        return "redirect:/resetpass";
+        return "redirect:/";
+
     }
 }
