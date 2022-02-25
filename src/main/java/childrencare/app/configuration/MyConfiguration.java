c0920f9dbcf6127a3a496624310e12feabed8cd1
Dinh Cong Thanh <thanhdche150982@fpt.edu.vn>
2022-02-25 21:59:36 +0700

    Multilanguage -v1

--- src/main/java/childrencare/app/configuration/MyConfiguration.java
+++ src/main/java/childrencare/app/configuration/MyConfiguration.java
@@ -1,5 +1,6 @@
 package childrencare.app.configuration;
 
+import java.util.Locale;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import childrencare.app.filter.AuthorizationFilter;
 import childrencare.app.filter.FirstAccessingFilter;
 
 @Configuration
-public class MyConfiguration {
+public class MyConfiguration implements WebMvcConfigurer{
 	private FirstAccessingFilter firstAccessingFilter;
 	private AuthorizationFilter authorizarionFilter;
 	@Autowired
@@ -55,5 +61,26 @@ public class MyConfiguration {
 	    props.put("mail.debug", "true");
 		return mailSender;
 	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.forLanguageTag("vi"));
+	    return slr;
+	}
+	
+	@Bean 
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	
 
 }
