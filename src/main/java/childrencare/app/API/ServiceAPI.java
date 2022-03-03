b12390968b02de3f61c548e381602edb8cb9c188
NguyenTheThai <thainthe150042@fpt.edu.vn>
2022-03-03 22:26:41 +0700

    service detail - manager (html + update) --> DONE

--- src/main/java/childrencare/app/API/ServiceAPI.java
+++ src/main/java/childrencare/app/API/ServiceAPI.java
@@ -2,13 +2,12 @@ package childrencare.app.API;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import childrencare.app.model.ServiceCategoryModel;
-import childrencare.app.service.ServiceCategoryService;
+import childrencare.app.repository.ServiceCategoryRepository;
 import org.springframework.web.bind.annotation.*;
 
 import childrencare.app.model.ServiceModel;
@@ -20,9 +19,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api-service")
 public class ServiceAPI {
 	private final ServiceRepository serviceRepository;
+	private final ServiceCategoryRepository serviceCategoryRepository;
 	
-	public ServiceAPI(ServiceRepository serviceRepository) {
+	public ServiceAPI(ServiceRepository serviceRepository, ServiceCategoryRepository serviceCategoryRepository) {
 		this.serviceRepository = serviceRepository;
+		this.serviceCategoryRepository = serviceCategoryRepository;
 	}
 	
 	@GetMapping("/get-user-services")
@@ -38,25 +39,24 @@ public class ServiceAPI {
 	public String updateService(@RequestParam(name = "title")String title, @RequestParam(name = "briefinfo")String briefInfo,
 								@RequestParam(name = "category")String category, @RequestParam(name = "originprice")double originPrice,
 								@RequestParam(name = "saleprice")double salePrice, @RequestParam(name = "description")String description,
-								@RequestParam(name = "id")int id){
+								@RequestParam(name = "id")int id, @RequestParam(name = "avatar",  required = false)MultipartFile file){
 		ServiceModel service = serviceRepository.getById(id);
-		//ServiceCategoryModel categoryModel = new ServiceCategoryModel();
+		ServiceCategoryModel categoryModel = serviceCategoryRepository.getServiceCategoryModelByName(category);
 		if(service != null){
 			service.setTitle(title);
 			service.setBriefInfo(briefInfo);
 			service.setOriginalPrice(originPrice);
 			service.setSalePrice(salePrice);
 			service.setDescription(description);
-			//categoryModel.setServiceCategoryName(category);
-			//service.setServiceCategory(categoryModel);
-		/*	if(file != null){
+			service.setServiceCategory(categoryModel);
+			if(file != null){
 				try {
 					service.setThumbnail(file.getBytes());
 					service.setBase64ThumbnailEncode(service.getThumbnail());
 				} catch (IOException e) {
 					return "fail";
 				}
-			}*/
+			}
 			serviceRepository.save(service);
 			return "success";
 
