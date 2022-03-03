8e00d7964715594f5d172c04fea6ff5187c9c065
NguyenTheThai <thainthe150042@fpt.edu.vn>
2022-03-03 21:41:42 +0700

    service detail - manager (html)

--- src/main/java/childrencare/app/API/ServiceAPI.java
+++ src/main/java/childrencare/app/API/ServiceAPI.java
@@ -1,15 +1,20 @@
 package childrencare.app.API;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import childrencare.app.model.ServiceCategoryModel;
+import childrencare.app.service.ServiceCategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import childrencare.app.model.ServiceModel;
 import childrencare.app.model.UserModel;
 import childrencare.app.repository.ServiceRepository;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api-service")
@@ -35,12 +40,23 @@ public class ServiceAPI {
 								@RequestParam(name = "saleprice")double salePrice, @RequestParam(name = "description")String description,
 								@RequestParam(name = "id")int id){
 		ServiceModel service = serviceRepository.getById(id);
+		//ServiceCategoryModel categoryModel = new ServiceCategoryModel();
 		if(service != null){
 			service.setTitle(title);
 			service.setBriefInfo(briefInfo);
 			service.setOriginalPrice(originPrice);
 			service.setSalePrice(salePrice);
 			service.setDescription(description);
+			//categoryModel.setServiceCategoryName(category);
+			//service.setServiceCategory(categoryModel);
+		/*	if(file != null){
+				try {
+					service.setThumbnail(file.getBytes());
+					service.setBase64ThumbnailEncode(service.getThumbnail());
+				} catch (IOException e) {
+					return "fail";
+				}
+			}*/
 			serviceRepository.save(service);
 			return "success";
 
