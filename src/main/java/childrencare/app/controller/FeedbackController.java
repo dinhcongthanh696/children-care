8aa7466c40e34c896b2c91b10067e582057899d4
AnhBDHE151175 <anhbdhe151175@fpt.edu.vn>
2022-03-12 00:55:48 +0700

    Commit feedback

--- src/main/java/childrencare/app/controller/FeedbackController.java
+++ src/main/java/childrencare/app/controller/FeedbackController.java
@@ -1,8 +1,11 @@
 package childrencare.app.controller;
 
+import childrencare.app.model.CustomerModel;
 import childrencare.app.model.FeedbackModel;
 import childrencare.app.model.ServiceModel;
+import childrencare.app.model.UserModel;
 import childrencare.app.repository.FeedbackRepository;
+import childrencare.app.service.CustomerService;
 import childrencare.app.service.FeedbackService;
 import childrencare.app.service.ServiceModelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
@@ -30,6 +34,9 @@ public class FeedbackController {
         this.serviceModelService = serviceModelService;
         this.feedbackService = feedbackService;
     }
+
+    @Autowired
+    private CustomerService customerService;
 
     @ResponseBody
     @RequestMapping("/sendSimpleEmail")
@@ -49,28 +56,34 @@ public class FeedbackController {
     }
 
     @GetMapping("/feedback")
-    public String feedbackHome() {
+    public String feedbackHome(Model model, HttpSession session) {
+        UserModel userModel = (UserModel) session.getAttribute("user");
+        model.addAttribute("user", userModel);
         return "contact";
     }
 
     @PostMapping("/feedback")
     @Transactional
-    public void feedbackHome(@RequestParam("fullname") String fullname,
-                               @RequestParam("gender") Integer get_gender,
-                               @RequestParam("email") String email,
-                               @RequestParam("phone") String phone,
-                               @RequestParam("myfile") MultipartFile myfile,
-                               @RequestParam(value = "rating", defaultValue = "0") Integer ratingstar,
-                               @RequestParam("message") String comment) throws IOException {
-        boolean gender = (get_gender == 1) ? true : false;
-        byte[] imageBinaryData = (myfile == null) ? null : myfile.getBytes();
-        feedbackService.saveGeneralFeedback(fullname, gender, email, phone, imageBinaryData, ratingstar, comment);
+    public void feedbackHome(
+            @RequestParam(name = "cus_email", defaultValue = "empty_email") String email,
+            @RequestParam(name = "create_date") String date,
+            @RequestParam(name = "image") MultipartFile img,
+            @RequestParam(name = "rating", defaultValue = "0") Integer ratingstar,
+            @RequestParam(name = "message") String comment) throws IOException {
+
+        int customer_id;
+        byte[] imageBinaryData = (img == null) ? null : img.getBytes();
+        if (!email.equals("empty_email")) {
+            customer_id = customerService.getCusIDByEmail(email);
+            feedbackService.saveGeneralFeedback(comment, date, imageBinaryData, ratingstar, true, customer_id);
+        } else {
+            feedbackService.saveGeneralFeedback(comment, date, imageBinaryData, ratingstar, true);
+        }
     }
 
 
-
     @GetMapping("/managerPage")
-    public String getManagerScreen(){
+    public String getManagerScreen() {
         return "manager-service-list";
     }
 
