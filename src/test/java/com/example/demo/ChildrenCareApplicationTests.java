e0a85b8b267c6ababe8b0e35be3e4242dc72a85b
Dinh Cong Thanh <thanhdche150982@fpt.edu.vn>
2023-10-02 14:22:29 +0700

    Add a test fail to see teamcity detect changes

--- src/test/java/com/example/demo/ChildrenCareApplicationTests.java
+++ src/test/java/com/example/demo/ChildrenCareApplicationTests.java
@@ -1,19 +1,32 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import childrencare.app.ChildrenCareApplication;
+import childrencare.app.model.DrugModel;
+import childrencare.app.service.DrugService;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = { ChildrenCareApplication.class })
 class ChildrenCareApplicationTests {
+	@Autowired
+	private DrugService drugService;
 
 	@Test
-	void contextLoads() {
-	}
+    public void testGetAllDrugs() {
+        List<DrugModel> drugModels = drugService.findAllDrugs();
+        // Use assertions to check if the result is as expected
+        assertEquals(drugModels.size(), 5);
+    }
 
 }
